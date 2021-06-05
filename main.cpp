87eb42c15dc08137e2a56a986b9e8d65dc08a6a2
Armando Hinojosa <sinpatrones1@gmail.com>
2021-06-04 23:10:17 -0500

    Se creo la funcion de aptitud, operacion cruce, operacion mutación

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <ctime>
 #include <stdlib.h>
 #include <cstdlib>
+#include <vector>
 
 using namespace std;
 
@@ -45,8 +46,9 @@ using namespace std;
 -------------
 
 */
+const int TAM = 11;
 
-char * generarIndividuo(int tam = 11){
+char * generarIndividuo(int tam = TAM){
     char * individuo = new char[tam];
 
     for (int idx = 0; idx < tam; idx++){
@@ -61,18 +63,70 @@ char * generarIndividuo(int tam = 11){
     return individuo;
 }
 
-void mostrarIndividuo(char * individuo, int tam){
+void mostrarIndividuo(char * individuo, int tam = TAM){
     for(int idx = 0; idx < tam; idx++){
         cout << individuo[idx];
     }
     cout << endl;
 }
 
+/* 010101  01111 */
+int funcionAptitud(char * individuo, int tam){
+    int inicio = (tam / 2) + 1;
+    int contarUnos = 0;
+    int contarCeros = 0;
+    for(int idx = 0; idx < inicio; idx++){
+        if (individuo[idx] == '1')
+            contarUnos++;
+    }
+    for(int idx = inicio; idx < tam; idx++){
+        if (individuo[idx] == '0')
+            contarCeros++;
+    }
+
+    return contarUnos + contarCeros;
+}
+
+bool operacionCruce(char * individuo1, char * individuo2, int tam = TAM){
+   // solo será una partición
+   int mitad = (tam / 2) + 1; // pseudo-mitad
+
+   char * primeraParte1 = new char[mitad];
+   // guardamos la cabeza del primero
+   for (int idx = 0; idx < mitad; idx++){
+       primeraParte1[idx] = individuo1[idx];
+   }
+   // pasamos la cabeza del segundo al primero
+   for (int idx = 0; idx < mitad; idx++){
+       individuo1[idx] = individuo2[idx];
+   }
+   // le ponemos la cabeza del individuo uno en dos
+   for (int idx = 0; idx < mitad; idx++){
+       individuo2[idx] = primeraParte1[idx];
+   }
+   return true;
+}
+
+bool operacionMutacion(char * individuo1, int tam = TAM){
+    int idx = rand() % tam;
+    if (individuo1[idx] == '1'){
+        individuo1[idx] = '0';
+    } else {
+        individuo1[idx] = '1';
+    }
+    return true;
+}
 
 int main(){
     srand(time(NULL));
 
-    mostrarIndividuo(generarIndividuo(), 11);
+    const int cantidadIndividuos = 10;
+    // cantidad de bits 11 <-- TAM
+    // 6 -> 1 >  11
+    // 5 -> 0 >
+
+
+
 
     return 0;
 }
